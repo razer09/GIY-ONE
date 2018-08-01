6882f70c7b4804c37fb5f6d89d8ba5b56171e5e6
guid <benkheddaacile@gmail.com>
2018-08-01 02:20:27 +0100

    fix bugs header

---
+++ indexx.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>PSD2Html[1]</title>
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        
+        <!-- Start Header -->
+        
+        <div class="header">
+            <div class="slider">
+                <div class="container">
+                    <div class="intro">
+                        <h2>Web &amp; Graphic Designer</h2>
+                        <button>Learn More</button>
+                    </div>
+                </div>
